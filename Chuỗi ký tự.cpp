ae848ea12a530b3b5ddc7cabe3016dd995b86dcb
normalguy464 <147421114+normalguy464@users.noreply.github.com>
2023-11-09 21:29:12 +0700

    Update Chuỗi ký tự.cpp

--- Chuỗi ký tự.cpp
+++ Chuỗi ký tự.cpp
@@ -1,4 +1,3 @@
-//Made by DangQuocDung
 #include<bits/stdc++.h>
 
 using namespace std;
@@ -10,12 +9,14 @@ int main(){
 	bool check = true;
 	int a[1000] = {0};
 	char b[1000];
+	int so_cau_hinh = 0; 
 	while(check){
 		int cnt = 0;
 		for(int i=0; i<=n; i++){
 			if(a[i+1]-a[i]==1 || a[i+1] - a[i] == -1) cnt++; //kiem tra tinh lien tuc cua cac ky tu A
 		}
 		if(cnt==2){
+			so_cau_hinh++;
 			for(int i=1; i<=n; i++){
 				if(a[i]==1) b[i] = 'A'; //chuyen cac chu so 1 thanh ky tu A
 				else b[i] = 'B'; // chuyen cac chu so 0 thanh ky tu B
@@ -35,4 +36,5 @@ int main(){
 		if(k==0) check = false;
 		// duyet cac xau nhi phan
 	}
+	cout << "So cau hinh thoa man la: " << so_cau_hinh;
 } 
