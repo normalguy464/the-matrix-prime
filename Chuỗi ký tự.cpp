66929e3552ceab707c4304a524de9e7d8bece4bb
normalguy464 <147421114+normalguy464@users.noreply.github.com>
2023-11-16 20:45:37 +0700

    Update Chuỗi ký tự.cpp

--- Chuỗi ký tự.cpp
+++ Chuỗi ký tự.cpp
@@ -12,10 +12,11 @@ int main(){
 	vector<char> v;
 	int so_cau_hinh = 0; 
 	while(check){
-		//kiem tra tinh lien tuc cua cac ky tu A
+		//cau hinh co dang a[1]a[2]...a[n]
+		//cau hinh dau tien la 000..0
 		int cnt = 0;
 		for(int i=0; i<=n; i++){
-			if(abs(a[i+1]-a[i])==1) cnt++; 
+			if(abs(a[i+1]-a[i])==1) cnt++; //kiem tra tinh lien tuc cua cac ky tu A
 		}
 		if(cnt==2){
 			so_cau_hinh++;
@@ -27,7 +28,6 @@ int main(){
 				v.push_back(b[i]);
 				}
 		}
-		//thuc hien duyet xau nhi phan ke tiep
 		int k = n;
 		while(a[k]==1 && k!=0){
 			a[k] = 0;
@@ -35,13 +35,12 @@ int main(){
 		}
 		a[k] = 1;
 		if(k==0) check = false;
-		}
-	//in ra so cau hinh thoa man
+		// duyet cac xau nhi phan
+	}
 	cout << "So cau hinh thoa man la: " << so_cau_hinh << endl;
-	//in ra cac cau hinh thoa man
 	int dem = 0;
 	for(auto x2 : v){
-		cout << x2; dem++; 
+		cout << x2; dem++;
 		if(dem==n){
 			 cout << endl;
 			 dem = 0;
